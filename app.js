4bf1d5701bb7276c6feb9706e662b0cdcf79ad1b
DDuAAtGhui <135325796+DDuAAtGhui@users.noreply.github.com>
2024-09-18 19:57:17 +0900

    add Basic Get Reqeust

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+const jokeAPI = "https://v2.jokeapi.dev";
+
+let result;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -13,9 +16,25 @@ app.set("view engine", "ejs");
 app.set("layout", "layout");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { result: result });
+});
+
+app.get("/getJoke", async (req, res) => {
+  result = await getJoke();
+  res.render("index", { result: result });
 });
 
 app.listen(port, () => {
   console.log(`포트번호 ${port}에서 서버 가동중`);
 });
+
+async function getJoke(category = "Any", lang = "en") {
+  try {
+    const result = await axios.get(`${jokeAPI}/joke/${category}`);
+    console.log(result.data);
+
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
